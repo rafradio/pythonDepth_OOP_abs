1645c4d0819bc696ffadd83356d9394e13debc8b
Rafail Abdyushev <rafaelab@mail.ru>
2023-07-11 17:44:56 +0300

    main prog

---
+++ matrixMain.py
@@ -0,0 +1,19 @@
+from collections.abc import MutableMapping
+from MatrixDict import MatrixDict
+
+def main():
+    m1 = MatrixDict(3, 3, 2)
+    m2 = MatrixDict(3, 3, 4)
+    m1[1,3] = 5
+    m1[2,2] = 10
+    m2[2,3] = 15
+    m2[3, 3] = 7
+    m3 = m1 * m2
+    print(m3)
+    print(m3.items())
+    # print(m1[2,3])
+    # obj[5,3] = 55
+    # print(m2[3,3])
+
+if __name__ == '__main__':
+    main()
